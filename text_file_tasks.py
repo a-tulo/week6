8bf6b426f3d1f67b3df65005c69957ac3ad49e04
alexandre.tulodziecki <alexandre.tulodziecki@yunextraffic.com>
2023-11-06 09:30:17 +0000

    Added code to demonstrate how to read a text file.

--- text_file_tasks.py
+++ text_file_tasks.py
@@ -6,10 +6,35 @@ def cwd():
     for file in os.listdir(path):
         print(file)
 
+def display_chars(path, num_char):
+    with open(path) as file:
+        data = file.read(num_char)
+    print(f"The first {num_char} characters are:")
+    print(data)
+
+def display_line(path):
+    with open(path) as file:
+        line = file.readline().strip()
+    print("The first line is:")
+    print(line)
+
+def display_text(path):
+    with open(path) as file:
+        data = file.read()
+    print("The full text is:")
+    print(data)
+
+def run_task2():
+    path = "library.txt"
+    display_chars(path, 5)
+    print()
+    display_line(path)
+    print()
+    display_text(path)
 
 def run():
     print("Processsing...")
     cwd()
 
 if __name__ == "__main__":
-    run()
+    run_task2()
