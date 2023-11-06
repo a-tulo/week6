f702b12db2f37fe079f95fadfda06f285811d61a
alexandre.tulodziecki <alexandre.tulodziecki@yunextraffic.com>
2023-11-06 11:19:05 +0000

    Added code to extract data from the CSV file.

--- csv_tasks.py
+++ csv_tasks.py
@@ -8,9 +8,23 @@ def read_csv(path):
         for values in csv_reader:
             print(values)
 
+def extract(path):
+    print("Extracting...")
+
+    with open(path) as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        names = ""
+        for values in csv_reader:
+            names += f"{values[1]}\n"
+    print("Done!")
+    print(f"The extracted items are: \n{names}")
+
 def run_task1():
     read_csv("clothing.csv")
+def run_task2():
+    extract("clothing.csv")
 
 
 if __name__ == "__main__":
-    run_task1()
+    run_task2()
